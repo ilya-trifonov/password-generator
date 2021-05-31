c2e29626e24bb5228c9502c512c591caa3d414a8
Ilya Trifonov <41755260+ilya-trifonov@users.noreply.github.com>
2021-05-31 19:01:23 +0300

    Update App.js
    some changes

--- src/App.js
+++ src/App.js
@@ -182,7 +182,7 @@ class App extends Component {
                               this.generatePwd();
                             }}
                           >
-                            Создать
+                            Сгенерировать
                           </Button>   
                       </div>               
                   </div>
