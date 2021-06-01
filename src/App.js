db4713b3c13c8d0ce6822254b13b50a3bf2529d2
ilya-trifonov <ilya.trifonov@uniquelab.ru>
2021-06-01 16:16:06 +0300

    changes

--- src/App.js
+++ src/App.js
@@ -255,7 +255,7 @@ class App extends Component {
                           this.generatePwd()
                         }}
                       >
-                        Редактирвоать пароль
+                        Создать
                       </Button>
                     </div>
                   </div>
